90ef93e006332ae21481c6c1f2d27b97b9d17015
soumyadepsinha <s.sinha2701@gmail.com>
2020-09-05 12:13:44 +0530

    update comments for javascript

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ menuToggler.addEventListener('click', function () {
 // Scroll Reveal
 window.sr = ScrollReveal();
 
+// animate the left side
 sr.reveal('.animate_left', {
   origin: 'left',
   duration: '1000',
@@ -19,6 +20,7 @@ sr.reveal('.animate_left', {
   delay: 300
 });
 
+// animate the right side
 sr.reveal('.animate_right', {
   origin: 'right',
   duration: '1000',
@@ -26,6 +28,7 @@ sr.reveal('.animate_right', {
   delay: 600
 });
 
+// animate top part
 sr.reveal('.animate_top', {
   origin: 'top',
   duration: '1000',
@@ -33,6 +36,7 @@ sr.reveal('.animate_top', {
   delay: 600
 });
 
+// animate bottom part
 sr.reveal('.animate_bottom', {
   origin: 'bottom',
   duration: '1000',
